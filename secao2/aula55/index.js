bd6e69f01524c34917e1ebb5cb8f2d9810ebdc4e
Gabriel Sales <gabrielsales07112018@gmail.com>
2023-11-25 14:16:52 -0300

    Adição de exercícios

---
+++ secao2/aula55/index.js
@@ -0,0 +1,5 @@
+const pessoa = {nome: 'Gabriel', sobrenome: 'Sales', idade: '22'}
+
+for (let i in pessoa) {
+    console.log(pessoa[i])
+}
---
+++ secao2/aula56/index.js
@@ -0,0 +1,5 @@
+const nomes = ['ZkSync', 'Starknet', 'Linea', 'Scroll']
+
+for (let valor of nomes) {
+    console.log(valor)
+}
