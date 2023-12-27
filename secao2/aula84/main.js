73d95cf106a668101b020d21ce3d9529cd0e8909
Gabriel Sales <gabrielsales07112018@gmail.com>
2023-12-27 07:37:11 -0300

    Terminei meu treino na aula84

--- secao2/aula84/main.js
+++ secao2/aula84/main.js
@@ -10,8 +10,15 @@ const pessoas = [
 const pessoasComMaisDe50 = pessoas.filter(obj => obj.idade > 50
 )
 
-console.log(pessoasComMaisDe50)
+nomesPessoasComMaisDe50 = []
+for (pessoa of pessoasComMaisDe50) {
+    const nomePessoaComMaisDe050 = pessoa.nome
+    nomesPessoasComMaisDe50.splice(0, 0, ` ${nomePessoaComMaisDe050}`)
+}
+console.log(`As pessoas com mais de 50 anos de idade são:${nomesPessoasComMaisDe50}.`)
+
 
 const pessoasComNomeTerminadoEmA = pessoas.filter(obj => obj.nome[obj.nome.length - 1] === 'a') // retorna as pessoas que tem a ultima letra igual a letra 'a' e adiciona o dicionário dessa pessoa na variavel 'pessoasComNomeTerminadoEmA'
 
+console.log('Pessoas cujo o primeiro nome terminam com a letra A:')
 console.log(pessoasComNomeTerminadoEmA)
