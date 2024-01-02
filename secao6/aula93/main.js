64938fbc5caccfba1cf4e21144974a6651b343ac
Gabriel Sales <gabrielsales07112018@gmail.com>
2024-01-02 09:14:21 -0300

    Aula sobre Prototype

---
+++ secao6/aula93/main.js
@@ -0,0 +1,12 @@
+function Pessoa(nome, sobrenome) {
+    this.nome = nome
+    this.sobrenome = sobrenome
+}
+
+Pessoa.prototype.nomeCompleto = function() {
+    return `${this.nome} ${this.sobrenome}`
+}
+
+const pessoa1 = new Pessoa('Gabriel', 'Sales')
+
+console.log(pessoa1.nomeCompleto())
