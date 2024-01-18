d4d65777c6a536a3f2ce61cb276b110a6e3ad56d
Gabriel Sales <gabrielsales07112018@gmail.com>
2024-01-17 21:17:02 -0300

    Aula sobre Ajax versão com Promise()

--- secao8/aula111/assets/js/main.js
+++ secao8/aula111/assets/js/main.js
@@ -1,14 +1,17 @@
 const request = obj => {
-    const xhr = new XMLHttpRequest() // instânciando o construtor
-    xhr.open(obj.method, obj.url, true) // configurando a requisição, informando o método e a url que localiza o servidor e etc...
-    xhr.send() // enviando a requisição que foi configurada
-
-    xhr.addEventListener('load', () => { // a função aqui será executada quando a requisição voltar do servidor e a página for carregada
-        if(xhr.status >= 200 && xhr.status < 300) { // verificando se o código http é um código da classe bem sucedido
-            obj.success(xhr.responseText) // xhr.responseText contém a resposta em forma de texto do servidor, nesse caso será o conteúdo da um página HTML
-        } else {
-            obj.error(xhr.statusText)
-        }
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest() // instânciando o construtor
+        xhr.open(obj.method, obj.url, true) // configurando a requisição, informando o método e a url que localiza o servidor e etc...
+        xhr.send() // enviando a requisição que foi configurada
+    
+        xhr.addEventListener('load', () => { // a função aqui será executada quando a requisição voltar do servidor e a página for carregada
+            if(xhr.status >= 200 && xhr.status < 300) { // verificando se o código http é um código da classe bem sucedido
+                resolve(xhr.responseText) // xhr.responseText contém a resposta em forma de texto do servidor, nesse caso será o conteúdo da um página HTML
+            } else {
+            const erro = `Error: ${xhr.status} - ${xhr.statusText}` // configurando mensagem de erro
+                reject(erro)
+            }
+        })
     })
 }
 
@@ -22,22 +25,22 @@ document.addEventListener('click', (e) => {
     }
 })
 
-function carregaPagina(el) {
+async function carregaPagina(el) {
     const href = el.getAttribute('href')
 
     const objConfig = { // objeto que contem as configurações da requisição que será enviada
         method: 'GET',
         url: href,
-
-        success(response) {
-            carregaResultado(response)
-        },
-        error(errorText) {
-            console.log(errorText)
-        }
     }
 
-    request(objConfig)
+    try {
+        request(objConfig)
+        const response = await request(objConfig)
+        carregaResultado(response)
+    
+    } catch(e) {
+        console.error(e)
+    }
 }
 
 function carregaResultado(response) {
