3774adaff5166e3f1aeddbc44e9a799c7c3e2af7
Gabriel Sales <gabrielsales07112018@gmail.com>
2024-01-28 14:02:20 -0300

    Terminando as configurações do Bundle

---
+++ secao9/aula116/public/assets/js/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('Deu certo');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
