cb4b63aff463be83ffb6727066c3021464c0f64c
Gabriel Sales <gabrielsales07112018@gmail.com>
2024-04-06 16:46:42 -0300

    Express session e Flash Messages

---
+++ secao10/aula142/server.js
@@ -0,0 +1,51 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.connectionString)
+    .then(() => {
+        app.emit('pronto')
+    })
+    .catch((e) => console.log(e))
+
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+const store = new MongoStore({
+    mongoUrl: process.env.connectionString,
+    collectionName: 'session'
+})
+const flash = require('connect-flash')
+
+const routes = require('./routes')
+const path = require('path')
+
+app.use(express.urlencoded({extended: true}))
+
+app.use(express.static(path.resolve(__dirname, 'public')))
+
+const sessionOptions = session({
+    secret: 'lorem ipsom',
+    store: store,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true
+    }
+})
+app.use(sessionOptions)
+app.use(flash())
+
+app.use(routes)
+
+app.set('views', path.resolve(__dirname, 'src', 'views'))
+app.set('view engine', 'ejs')
+
+app.on('pronto', () => {
+    app.listen(3000, () => {
+        console.log('Acessar http://localhost:3000')
+        console.log('Acessando meu servidor na porta 3000')
+    })
+})
