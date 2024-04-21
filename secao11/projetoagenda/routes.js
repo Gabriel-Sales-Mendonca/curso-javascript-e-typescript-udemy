a0a756f8b55e2ad52dc2d18639d04ac69ce78809
Gabriel Sales <gabrielsales07112018@gmail.com>
2024-04-21 19:33:41 -0300

    Criando contato

--- secao11/projetoagenda/routes.js
+++ secao11/projetoagenda/routes.js
@@ -4,6 +4,7 @@ const route = express.Router()
 const homeController = require('./src/controller/homeControllers')
 const loginController = require('./src/controller/loginControllers')
 const contatoController = require('./src/controller/contatoController')
+const middleware = require('./src/middlewares/middleware')
 
 //rotas de home
 route.get('/', homeController.index)
@@ -15,6 +16,8 @@ route.post('/login/login', loginController.login)
 route.get('/login/logout', loginController.logout)
 
 //rotas de contato
-route.get('/contato/index', contatoController.index)
+route.get('/contato/index', middleware.checkLogin, contatoController.index)
+route.post('/contato/register', middleware.checkLogin, contatoController.register)
+route.get('/contato/index/:id', middleware.checkLogin, contatoController.editIndex)
 
 module.exports = route
--- secao11/projetoagenda/src/controller/contatoController.js
+++ secao11/projetoagenda/src/controller/contatoController.js
@@ -1,3 +1,38 @@
+const Contato = require('../models/ContatoModel')
+
 exports.index = (req, res) => {
-    res.send('página de contato')
+    res.render('contato')
+}
+
+exports.register = async (req, res) => {
+    try {
+        const contato = new Contato(req.body)
+        await contato.register()
+    
+        if(contato.errors.length > 0) {
+            req.flash('errors', contato.errors)
+            req.session.save(function() {
+                return res.redirect('back')
+            })
+            return
+        }
+    
+        req.flash('success', 'Seu contato foi criado com sucesso!')
+        req.session.save(function() {
+            return res.redirect(`/contato/index/${contato.contato._id}`)
+        })
+    } catch(e) {
+        console.log(e)
+        return res.render('404')
+    }
+}
+
+exports.editIndex = async (req, res) => {
+    if(!req.params.id) return res.render('404')
+
+    const contato = await Contato.searchById(req.params.id)
+
+    if(!contato) return res.render('404')
+
+    res.render('contato', { contato })
 }
