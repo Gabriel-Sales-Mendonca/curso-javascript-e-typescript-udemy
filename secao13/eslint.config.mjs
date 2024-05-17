f8b4c4cfb14a7205bdf35d5c3b478e99db903b14
Gabriel Sales <gabrielsales07112018@gmail.com>
2024-05-17 19:53:30 -0300

    Criei a rota '/' e estruturei o projeto

--- secao13/eslint.config.mjs
+++ secao13/eslint.config.mjs
@@ -3,6 +3,14 @@ import pluginJs from "@eslint/js";
 
 
 export default [
-  {languageOptions: { globals: globals.node }},
+  {languageOptions: { globals: globals.node },
+    /*env: {
+      es6: true,
+      node: true,
+    },*/
+    rules: {
+
+    }
+  },
   pluginJs.configs.recommended,
 ];
--- secao13/src/routes/homeRoutes.js
+++ secao13/src/routes/homeRoutes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express'
+import homeController from '../controllers/HomeController'
 
 const router = new Router()
 
-router.get('/', (re, res) => {
-  res.send('Olá mundo!')
+router.get('/', homeController.index)
+
+router.get('/teste', (req, res) => {
+  res.send('cheguei')
 })
 
 export default router
 
-router.get()
