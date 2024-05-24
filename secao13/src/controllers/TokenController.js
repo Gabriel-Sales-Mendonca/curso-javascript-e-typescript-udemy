088fb96281e86734f798b7b8afb0300aa0b3f87b
Gabriel Sales <gabrielsales07112018@gmail.com>
2024-05-24 13:15:00 -0300

    criei a rota do token e criei o controller do token

--- secao13/src/controllers/TokenController.js
+++ secao13/src/controllers/TokenController.js
@@ -1,8 +1,37 @@
 import User from '../models/User'
+import jwt from 'jsonwebtoken'
 
 class TokenController {
-  async index(req, res) {
-    res.json('ok')
+  async store(req, res) {
+    const { email = '', password = '' } = req.body
+
+    if(!email || !password) {
+      return res.status(401).json({
+        errors: ['Um dos valores está em branco.']
+      })
+    }
+
+    const user = await User.findOne({ where: { email: email } })
+
+    if(!user) {
+      return res.status(401).json({
+        errors: ['Usuário não existe.']
+      })
+    }
+
+    if(!(await user.passwordIsValid(password))) {
+      return res.status(401).json({
+        errors: ['Senha inválida.']
+      })
+    }
+
+    const { id } = user
+
+    const token = jwt.sign({id, email}, process.env.TOKEN_SECRET, {
+      expiresIn: process.env.TOKEN_EXPIRATION
+    })
+
+    return res.json({ token })
   }
 }
 
