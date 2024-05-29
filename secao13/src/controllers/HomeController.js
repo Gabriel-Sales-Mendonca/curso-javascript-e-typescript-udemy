be9a0b69a03410a16d33188e31788eebf88d24f3
Gabriel Sales <gabrielsales07112018@gmail.com>
2024-05-28 23:49:57 -0300

    Arrumei a rota da home

--- secao13/src/controllers/HomeController.js
+++ secao13/src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno'
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Gabriel',
-      sobrenome: 'Sales',
-      email: 'gabriel@gmail.com',
-      idade: 22,
-      peso: 55,
-      altura: 1.72
-    })
-    res.json(novoAluno)
+    res.json('Index')
   }
 }
 
