0c19dbc61f2b66db3be141a4460b04303045ecdf
Gabriel Sales <gabrielsales07112018@gmail.com>
2024-05-30 12:16:50 -0300

    Fiz o CRUD de alunos

--- secao13/src/models/Aluno.js
+++ secao13/src/models/Aluno.js
@@ -3,12 +3,62 @@ import Sequelize, { Model } from 'sequelize';
 export default class Aluno extends Model {
   static init(sequelize) {
     super.init({
-      nome: Sequelize.STRING,
-      sobrenome: Sequelize.STRING,
-      email: Sequelize.STRING,
-      idade: Sequelize.INTEGER,
-      peso: Sequelize.FLOAT,
-      altura: Sequelize.FLOAT
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'Nome precisa ter entre 3 e 255 caracteres.'
+          }
+        }
+      },
+
+      sobrenome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'Sobrenome precisa ter entre 3 e 255 caracteres.'
+          }
+        }
+      },
+
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: {
+          msg: 'Email já existe.'
+        },
+        isEmail: {
+          msg: 'Email inválido.'
+        }
+      },
+
+      idade: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        isInt: {
+          msg: 'A idade precisa ser um número.'
+        }
+      },
+
+      peso: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+        isFloat: {
+          msg: 'O peso precisa ser um número.'
+        }
+      },
+
+      altura: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+        isFloat: {
+          msg: 'A altura precisa ser um número.'
+        }
+      }
     }, {
       sequelize
     })
--- secao13/src/routes/alunoRoutes.js
+++ secao13/src/routes/alunoRoutes.js
@@ -4,5 +4,9 @@ import alunoController from '../controllers/AlunoController'
 const route = new Router()
 
 route.get('/', alunoController.index)
+route.get('/:id', alunoController.show)
+route.post('/', alunoController.store)
+route.put('/:id', alunoController.update)
+route.delete('/:id', alunoController.delete)
 
 export default route
