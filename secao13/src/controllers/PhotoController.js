56b8caf349b1d262322f55f5786b5e98e9c464a2
Gabriel Sales <gabrielsales07112018@gmail.com>
2024-06-01 10:15:56 -0300

    Criei o relacionamento da tabela de photos com a tabela de alunos

--- secao13/src/controllers/PhotoController.js
+++ secao13/src/controllers/PhotoController.js
@@ -1,20 +1,27 @@
 import multer from 'multer'
 import multerConfig from '../config/multer'
+import Photo from '../models/Photo'
+//import Aluno from '../models/Aluno'
 
 const upload = multer(multerConfig).single('photo')
 
-class Photo {
-  async store(req, res) {
-    return upload(req, res, (error) => {
+class PhotoController {
+  store(req, res) {
+    return upload(req, res, async (error) => {
       if(error) {
         return res.status(400).json({
           errors: [error.code]
         })
       }
 
-      return res.json(req.file)
+      const { originalname, filename } = req.file
+      const { alunos_id } = req.body
+
+      const photo = await Photo.create({ originalname, filename, alunos_id })
+
+      return res.json(photo)
     })
   }
 }
 
-export default new Photo()
+export default new PhotoController()
