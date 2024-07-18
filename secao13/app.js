83f4b72b559c6b075e5fcdc620a0b32cfc7d27b6
Gabriel Sales <gabrielsales07112018@gmail.com>
2024-07-18 12:41:13 -0300

    Retornando informações do usuário na rota de geração de token

--- secao13/app.js
+++ secao13/app.js
@@ -6,6 +6,8 @@ dotenv.config()
 import './src/database'
 
 import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
 import homeRoutes from './src/routes/homeRoutes'
 import userRoutes from './src/routes/userRoutes'
 import tokenRoutes from './src/routes/tokenRoutes'
@@ -20,6 +22,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors())
+    this.app.use(helmet())
     this.app.use(express.urlencoded({ extended: true }))
     this.app.use(express.json())
     this.app.use(express.static(resolve(__dirname, 'uploads')))
