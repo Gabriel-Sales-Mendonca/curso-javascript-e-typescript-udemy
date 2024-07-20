7e0f6e092a2c5978ad2f1864bd21c5037b585584
Gabriel Sales <gabrielsales07112018@gmail.com>
2024-07-20 12:02:22 -0300

    Criei a nav da página e adicionei o pacote react-icons

--- secao15/react-base/src/App.js
+++ secao15/react-base/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
+import Header from './components/Header'
 import Login from './pages/Login/index'
 import GlobalStyle from './styles/GlobalStyles';
 
 function App() {
   return (
     <>
+      <Header />
       <Login />
       <GlobalStyle />
     </>
