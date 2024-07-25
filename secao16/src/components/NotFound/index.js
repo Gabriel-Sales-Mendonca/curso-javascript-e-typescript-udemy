f62bca913959dced6386d8689d29647e41ca0a95
Gabriel Sales <gabrielsales07112018@gmail.com>
2024-07-25 07:23:55 -0300

    Adicionei uma rota para pagina não encontrada

---
+++ secao16/src/components/NotFound/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Message } from './styled'
+
+export default function NotFound() {
+    return (
+        <Message>
+            <h1>Página não encontrada!</h1>
+        </Message>
+    )
+}
---
+++ secao16/src/components/NotFound/styled.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const Message = styled.div`
+    background-color: black;
+    color: red;
+`
