cc1efbb605809ba62fdf458dd70b0792489b13e0
Gabriel Sales <gabrielsales07112018@gmail.com>
2024-07-25 12:37:45 -0300

    Criei um rota de redirect

---
+++ secao16/src/components/Redirect/index.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+export default function Redirect() {
+    const [time, setTime] = useState(3)
+    const timeout = useRef(0)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        clearTimeout(timeout.current)
+
+        timeout.current = setTimeout(() => {
+            setTime((t) => t-1)
+        }, 1000)
+
+        if(time <= 0) {
+            navigate('/')
+        }
+
+        return () => {
+            clearTimeout(timeout.current)
+        }
+    }, [time])
+
+
+    return (
+        <>
+            <h1>Você será redirecioado em: {time}</h1>
+        </>
+    )
+}
---
+++ secao16/src/components/Redirect/styled.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const List = styled.nav`
+    background-color: blue;
+
+    ul {
+        display: flex;
+        list-style: none;
+        gap: 60px;
+    }
+    
+    a {
+        color: white;
+        text-decoration: none;
+    }
+`
