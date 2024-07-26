0a75a48ed123ceba727da5ce05e8cd425a706ee1
Gabriel Sales <gabrielsales07112018@gmail.com>
2024-07-26 07:33:28 -0300

    Configurei o axios na aplicação consumindo a api

--- secao15/react-base/src/pages/Login/index.js
+++ secao15/react-base/src/pages/Login/index.js
@@ -3,9 +3,18 @@ import { toast } from 'react-toastify'
 
 import { Title } from './styled'
 import { Container } from '../../styles/GlobalStyles'
+import axios from '../../services/axios'
 
 export default function Login() {
     toast.success('Você está em Home Page')
+    React.useEffect(() => {
+        async function getData() {
+            const response = await axios.get('/alunos')
+            console.log(response)
+        }
+
+        getData()
+    }, [])
 
     return (
         <Container>
