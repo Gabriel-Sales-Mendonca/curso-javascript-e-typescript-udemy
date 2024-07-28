970efbec9076b29817878b0dbe1ed16d1dd87ad7
Gabriel Sales <gabrielsales07112018@gmail.com>
2024-07-27 22:23:26 -0300

    Organizei a estrutura do redux

--- secao15/react-base/src/components/Header/index.js
+++ secao15/react-base/src/components/Header/index.js
@@ -6,14 +6,14 @@ import { useSelector } from 'react-redux'
 import { Nav } from './styled'
 
 export default function Header() {
-    const botaoClicado = useSelector(state => state.botaoClicado)
+    const buttonClicked = useSelector(state => state.example.buttonClicked)
 
     return (
         <Nav>
             <Link to='/'><FaHome size={24}/></Link>
             <Link to='/login'><FaUserAlt size={24}/></Link>
             <Link to='/exit'><FaSignInAlt size={24}/></Link>
-            {botaoClicado ? 'Clicado' : 'Não clicado'}
+            {buttonClicked ? 'Clicado' : 'Não clicado'}
         </Nav>
     )
 }
