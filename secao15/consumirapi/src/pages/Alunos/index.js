ca96b5d435426d7bd0203b88e682c5dafd17c928
Gabriel Sales <gabrielsales07112018@gmail.com>
2024-07-31 07:29:52 -0300

    Comecei a estilizar a página de alunos

--- secao15/consumirapi/src/pages/Alunos/index.js
+++ secao15/consumirapi/src/pages/Alunos/index.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { get } from 'lodash'
+import { FaUserCircle } from 'react-icons/fa'
 
 import { Container } from '../../styles/GlobalStyles'
+import axios from '../../services/axios'
+import { AlunoContainer, ProfilePicture } from './styled'
 
 export default function Alunos() {
+    const [alunos, setAlunos] = useState([])
+
+    useEffect(() => {
+        async function getData() {
+            const response = await axios.get('/alunos')
+            setAlunos(response.data)
+        }
+
+        getData()
+    }, [])
+
     return (
         <Container>
             <h1>Alunos</h1>
+
+            <AlunoContainer>
+                {alunos.map((aluno) => (
+                    <div key={String(aluno.id)}>
+                        <ProfilePicture>
+                            {get(aluno, 'Photos[0].url', false) ? (
+                                <img src={aluno.Photos[0].url} alt='' />
+                            ) : (
+                                <FaUserCircle size={36} />
+                            )}
+                        </ProfilePicture>
+
+                        <span>{aluno.nome}</span>
+                        <span>{aluno.sobrenome}</span>
+                        <span>{aluno.email}</span>
+                    </div>
+                ))}
+            </AlunoContainer>
         </Container>
     )
 }
--- secao15/consumirapi/src/pages/Alunos/styled.js
+++ secao15/consumirapi/src/pages/Alunos/styled.js
@@ -1,3 +1,21 @@
 import styled from 'styled-components'
 
-export const Title = styled.h1``
+export const AlunoContainer = styled.div`
+    margin-top: 20px;
+
+    div {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 5px 0;
+        border-bottom: 1px solid #CDCDCD;
+    }
+`
+
+export const ProfilePicture = styled.section`
+    img {
+        width: 36px;
+        height: 36px;
+        border-radius: 50%;
+    }
+`
