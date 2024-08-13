c7713ddd60e4422cca467d840d52c4f2b1db404e
Gabriel Sales <gabrielsales07112018@gmail.com>
2024-08-13 07:21:15 -0300

    Fiz a função de excluir um aluno se o usuário estiver logado

--- secao15/consumirapi/src/pages/Alunos/index.js
+++ secao15/consumirapi/src/pages/Alunos/index.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { get } from 'lodash'
-import { FaUserCircle, FaEdit, FaWindowClose } from 'react-icons/fa'
+import { FaUserCircle, FaEdit, FaWindowClose, FaExclamation } from 'react-icons/fa'
+import { toast } from 'react-toastify'
 
 import { Container } from '../../styles/GlobalStyles'
 import axios from '../../services/axios'
@@ -23,13 +24,42 @@ export default function Alunos() {
         getData()
     }, [])
 
+    const handleDeletAsk = (e) => {
+        e.preventDefault()
+        const exclamation = e.currentTarget.nextSibling
+        exclamation.setAttribute('display', 'block')
+        e.currentTarget.remove()
+    }
+
+    const handleDelete = async (e, id, index) => {
+        e.persist()
+        
+        try {
+            setIsLoading(true)
+            await axios.delete(`/alunos/${id}`)
+            const novosAlunos = [ ...alunos ]
+            novosAlunos.splice(index, 1)
+            setAlunos(novosAlunos)
+            setIsLoading(false)
+        } catch(err) {
+            const status = get(err, 'response.status', [])
+            
+            if(status === 401) {
+                toast.error('Você precisa fazer login')
+            } else {
+                toast.error('Ocorreu um erro ao excluir aluno')
+            }
+            setIsLoading(false)
+        }
+    }
+
     return (
         <Container>
             <Loading isLoading={isLoading} />
             <h1>Alunos</h1>
             
             <AlunoContainer>
-                {alunos.map((aluno) => (
+                {alunos.map((aluno, index) => (
                     <div key={String(aluno.id)}>
                         <ProfilePicture>
                             {get(aluno, 'Photos[0].url', false) ? (
@@ -45,9 +75,11 @@ export default function Alunos() {
                         <Link to={`/aluno/${aluno.id}/edit`} >
                             <FaEdit size={16} />
                         </Link>
-                        <Link to={`/aluno/${aluno.id}/delete`} >
+                        <Link onClick={handleDeletAsk} to={`/aluno/${aluno.id}/delete`} >
                             <FaWindowClose size={16} />
                         </Link>
+
+                        <FaExclamation onClick={e => handleDelete(e, aluno.id, index)} size={16} display="none" cursor="pointer" />
                     </div>
                 ))}
             </AlunoContainer>
